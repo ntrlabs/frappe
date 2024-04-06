a42c4b57fd7313bddf7e21847a81b649be95b968
Ankush Menat <ankush@frappe.io>
2024-04-06 20:18:50 +0530

    refactor: use Fetch API instead of superagent
    (cherry picked from commit 266b2797aaf2a5abe344c1d812784bba7413402a)
    
    # Conflicts:
    #	realtime/utils.js

--- realtime/utils.js
+++ realtime/utils.js
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 const request = require("superagent");
+=======
+const { get_conf } = require("../node_utils");
+const conf = get_conf();
+>>>>>>> 266b2797aa (refactor: use Fetch API instead of superagent)
 
 function get_url(socket, path) {
 	if (!path) {
@@ -7,17 +12,6 @@ function get_url(socket, path) {
 	return socket.request.headers.origin + path;
 }
 
-// Authenticates a partial request created using superagent
-function frappe_request(path, socket) {
-	const partial_req = request.get(get_url(socket, path));
-	if (socket.sid) {
-		return partial_req.query({ sid: socket.sid });
-	} else if (socket.authorization_header) {
-		return partial_req.set("Authorization", socket.authorization_header);
-	}
-}
-
 module.exports = {
 	get_url,
-	frappe_request,
 };
